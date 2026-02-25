19347a31b59c4736de10a1970c4c360afbf0964d
nesqual <nesqual@protonmail.com>
2026-02-24 21:46:28 -0500

    Added page indicator

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import {
+    BrowserRouter,
+    Link,
+    Route,
+    Routes,
+    useLocation,
+} from 'react-router-dom'
 import './index.scss'
 import './global-nav.scss'
 import Home from './components/main-pages/Home.tsx'
@@ -10,52 +16,85 @@ import Articles from './components/main-pages/Articles.tsx'
 import ArticleViewer from './components/main-pages/ArticleViewer.tsx'
 
 export default function App() {
+    const location = useLocation()
+    const [currentPage, setCurrentPage] = useState('')
+
+    useEffect(() => {
+        const path = location.pathname
+        const articlesRegex = /^\/articles(\/.*)?$/
+        if (path === '/') {
+            setCurrentPage('Home')
+        } else if (articlesRegex.test(path)) {
+            setCurrentPage('Articles')
+        } else if (path === '/about') {
+            setCurrentPage('About')
+        } else {
+            setCurrentPage('')
+        }
+    })
+
     return (
-        <StrictMode>
-            <BrowserRouter>
-                <div id='global-container'>
-                    <nav className='global-nav-header'>
-                        <Link id='nav-logo-wrapper' to='/'>
-                            <img
-                                id='nav-logo'
-                                width={50}
-                                height={50}
-                                src={NesqualStarbird}
-                                title={'Fight the Empire!'}
-                            />
+        <div id='global-container'>
+            <nav className='global-nav-header'>
+                <Link id='nav-logo-wrapper' to='/'>
+                    <img
+                        id='nav-logo'
+                        width={50}
+                        height={50}
+                        src={NesqualStarbird}
+                        title={'Fight the Empire!'}
+                    />
+                </Link>
+                <ul className='global-nav-list'>
+                    <li>
+                        <Link
+                            to='/'
+                            className={currentPage === 'Home' ? 'active' : ''}
+                        >
+                            Home
                         </Link>
-                        <ul className='global-nav-list'>
-                            <li>
-                                <Link to='/'>Home</Link>
-                            </li>
-                            <li>
-                                <Link to='/articles'>Articles</Link>
-                            </li>
-                            <li>
-                                <Link to='/about'>About</Link>
-                            </li>
-                        </ul>
-                        <div className='global-nav-right'>
-                            <Link id='global-nav-right-text' to='/'>
-                                Nesqual
-                            </Link>
-                        </div>
-                    </nav>
-                    <div id='global-content-window'>
-                        <Routes>
-                            <Route path='/' element={<Home />} />
-                            <Route path='/articles' element={<Articles />} />
-                            <Route path='/about' element={<About />} />
-                            <Route
-                                path='/articles/:id'
-                                element={<ArticleViewer />}
-                            />
-                        </Routes>
-                    </div>
+                    </li>
+                    <li>
+                        <Link
+                            to='/articles'
+                            className={
+                                currentPage === 'Articles' ? 'active' : ''
+                            }
+                        >
+                            Articles
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='/about'
+                            className={currentPage === 'About' ? 'active' : ''}
+                        >
+                            About
+                        </Link>
+                    </li>
+                </ul>
+                <div className='global-nav-right'>
+                    <Link id='global-nav-right-text' to='/'>
+                        Nesqual
+                    </Link>
                 </div>
-            </BrowserRouter>
-        </StrictMode>
+            </nav>
+            <div id='global-content-window'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/articles' element={<Articles />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/articles/:id' element={<ArticleViewer />} />
+                </Routes>
+            </div>
+        </div>
     )
 }
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
+)
