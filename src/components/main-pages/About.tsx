11cd12aeb913452f219b4331ade15e17045c93d7
nesqual <nesqual@protonmail.com>
2026-02-25 00:23:20 -0500

    Updated about page styling

--- src/components/main-pages/About.tsx
+++ src/components/main-pages/About.tsx
@@ -4,8 +4,12 @@ import NesqualStarbird from '../../assets/images/Nesqual_Starbird_2-plain-no-bac
 function About() {
     return (
         <div id='about-page'>
-            <h2 className='about-title'>Nesqual is a project by Nesqual.</h2>
-            <img className='nesqual-starbird' src={NesqualStarbird} />
+            <div id='about-wrapper'>
+                <h2 className='about-title'>
+                    Nesqual is a project by Nesqual.
+                </h2>
+                <img className='nesqual-starbird' src={NesqualStarbird} />
+            </div>
         </div>
     )
 }
